a02781016e524e92fab2485ffc62bf3b2d5aca54
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-01-31 20:48:01 -0500

    initial commit3

--- src/card/Card.java
+++ src/card/Card.java
@@ -10,7 +10,7 @@ package card;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
- * @Tharusha Henegama Liyanage
+ * @modifier Tharusha Henegama Liyanage
  * @author srinivsi
  */
 public class Card {
